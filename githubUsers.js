1d193c26d0b482a5361a514367c89c17af5940f4
isaacMwesigwa <mwesigwai433@gmail.com>
2024-07-20 21:09:11 +0300

    hello

--- githubUsers.js
+++ githubUsers.js
@@ -36,6 +36,12 @@ function getUsers() {
                             <li>Id:  ${users[i].id}</li>
                             <li>Name:  ${users[i].login}</li>
                         </ul>
+                        <div class="icons">
+                            <img src = "/like.svg" class="likeIcon" onclick="likeBtnClicked(event)">
+                            <img src = "/heart.svg" class="likeIcon" onclick = "loveBtnClicked(event)">
+                            <span>23</span>
+                            <span id="likes">${likeCount}</span>
+                        </div>
                     </div>
                 `
                 userDetails += user;
